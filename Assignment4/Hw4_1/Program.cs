5071dede805aeaf678f19ab11643141b3627d7a2
宁明宇 <1916977847@qq.com>
2024-03-27 00:15:08 +0800

    软构第四次作业

---
+++ Assignment4/Hw4_1/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+public class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    public Node<T> Head { get; private set; }
+    public Node<T> Tail { get; private set; }
+    public int Count { get; private set; }
+
+    public LinkedList()
+    {
+        Head = null;
+        Tail = null;
+        Count = 0;
+    }
+
+    public void Add(T value)
+    {
+        var newNode = new Node<T>(value);
+        if (Count == 0)
+        {
+            Head = newNode;
+            Tail = newNode;
+        }
+        else
+        {
+            Tail.Next = newNode;
+            Tail = newNode;
+        }
+        Count++;
+    }
+
+    // 添加ForEach方法  
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current = Head;
+        while (current != null)
+        {
+            action(current.Value);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> linkedList = new LinkedList<int>();
+        linkedList.Add(1);
+        linkedList.Add(5);
+        linkedList.Add(3);
+        linkedList.Add(7);
+        linkedList.Add(2);
+
+        // 打印链表元素  
+        linkedList.ForEach(element => Console.WriteLine(element));
+
+        // 求最大值  
+        int max = linkedList.ForEachMax(element => element);
+        Console.WriteLine("Max value: " + max);
+
+        // 求最小值  
+        int min = linkedList.ForEachMin(element => element);
+        Console.WriteLine("Min value: " + min);
+
+        // 求和  
+        int sum = linkedList.ForEachSum(element => element);
+        Console.WriteLine("Sum: " + sum);
+    }
+}
+
+// 为LinkedList<T>类添加求最大值、最小值和求和的扩展方法  
+public static class LinkedListExtensions
+{
+    public static T ForEachMax<T>(this LinkedList<T> linkedList, Func<T, T> selector) where T : IComparable<T>
+    {
+        T max = default(T);
+        bool hasValue = false;
+        linkedList.ForEach(element =>
+        {
+            T current = selector(element);
+            if (!hasValue || current.CompareTo(max) > 0)
+            {
+                max = current;
+                hasValue = true;
+            }
+        });
+        return max;
+    }
+
+    public static T ForEachMin<T>(this LinkedList<T> linkedList, Func<T, T> selector) where T : IComparable<T>
+    {
+        T min = default(T);
+        bool hasValue = false;
+        linkedList.ForEach(element =>
+        {
+            T current = selector(element);
+            if (!hasValue || current.CompareTo(min) < 0)
+            {
+                min = current;
+                hasValue = true;
+            }
+        });
+        return min;
+    }
+
+    public static T ForEachSum<T>(this LinkedList<T> linkedList, Func<T, T> selector) where T : struct
+    {
+        dynamic sum = 0; // 使用dynamic来处理不同的数值类型  
+        linkedList.ForEach(element =>
+        {
+            sum += selector(element);
+        });
+        return (T)sum;
+    }
+}
---
+++ Assignment4/Hw4_2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Timers;
+
+public class Clock
+{
+    // 定义一个委托，用于处理Tick事件  
+    public delegate void TickEventHandler(object source, EventArgs e);
+    // 定义一个事件，当闹钟走时触发  
+    public event TickEventHandler Tick;
+
+    // 定义一个委托，用于处理Alarm事件  
+    public delegate void AlarmEventHandler(object source, EventArgs e);
+    // 定义一个事件，当闹钟响铃时触发  
+    public event AlarmEventHandler Alarm;
+
+    // 创建一个计时器来模拟时钟的嘀嗒声  
+    private System.Timers.Timer _tickTimer;
+    // 闹钟响铃时间（假设是60秒后）  
+    private int _alarmTimeInSeconds = 60;
+    // 当前时间（秒）  
+    private int _currentTimeInSeconds = 0;
+
+    public Clock()
+    {
+        // 初始化计时器，间隔为1秒（1000毫秒）  
+        _tickTimer = new System.Timers.Timer(1000);
+        _tickTimer.Elapsed += OnTimedEvent;
+        // 开始计时器  
+        _tickTimer.Enabled = true;
+        _tickTimer.AutoReset = true;
+    }
+
+    public int CurrentTimeInSeconds
+    {
+        get { return _currentTimeInSeconds; }
+    }
+
+    // 当计时器触发时调用的方法  
+    private void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        _currentTimeInSeconds++;
+        // 触发Tick事件  
+        OnTick(EventArgs.Empty);
+
+        // 检查是否到达响铃时间  
+        if (_currentTimeInSeconds >= _alarmTimeInSeconds)
+        {
+            // 停止计时器  
+            _tickTimer.Stop();
+            // 触发Alarm事件  
+            OnAlarm(EventArgs.Empty);
+        }
+    }
+
+    // 保护型方法，用于触发Tick事件  
+    protected virtual void OnTick(EventArgs e)
+    {
+        Tick?.Invoke(this, e);
+        Console.WriteLine("嘀嗒...");
+    }
+
+    // 保护型方法，用于触发Alarm事件  
+    protected virtual void OnAlarm(EventArgs e)
+    {
+        Alarm?.Invoke(this, e);
+        Console.WriteLine("闹钟响了！");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Clock clock = new Clock();
+
+        // 订阅Tick事件  
+        clock.Tick += (sender, e) =>
+        {
+            Console.WriteLine("当前时间（秒）: " + clock.CurrentTimeInSeconds);
+        };
+
+        // 订阅Alarm事件  
+        clock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine("该起床了！");
+        };
+
+        Console.WriteLine("按任意键退出...");
+        Console.ReadKey();
+    }
+}
